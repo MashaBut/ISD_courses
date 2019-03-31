18d732a4b4abe042c1a159b1094359af62967629
MashaBut <42973147+MashaBut@users.noreply.github.com>
2019-03-31 12:08:29 +0300

    Designer

--- Cryptographer/Cryptographer/Form1.Designer.cs
+++ Cryptographer/Cryptographer/Form1.Designer.cs
@@ -34,9 +34,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.OutputText = new System.Windows.Forms.TextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Decipher = new System.Windows.Forms.RadioButton();
+            this.Encrypt = new System.Windows.Forms.RadioButton();
+            this.Apply = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -91,41 +91,42 @@
             this.OutputText.Size = new System.Drawing.Size(569, 180);
             this.OutputText.TabIndex = 3;
             // 
-            // radioButton1
+            // Decipher
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Sitka Small", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton1.Location = new System.Drawing.Point(67, 254);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(119, 32);
-            this.radioButton1.TabIndex = 5;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Decipher\r";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.Decipher.AutoSize = true;
+            this.Decipher.Font = new System.Drawing.Font("Sitka Small", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Decipher.Location = new System.Drawing.Point(67, 254);
+            this.Decipher.Name = "Decipher";
+            this.Decipher.Size = new System.Drawing.Size(119, 32);
+            this.Decipher.TabIndex = 5;
+            this.Decipher.TabStop = true;
+            this.Decipher.Text = "Decipher\r";
+            this.Decipher.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // Encrypt
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Sitka Small", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton2.Location = new System.Drawing.Point(192, 254);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(108, 32);
-            this.radioButton2.TabIndex = 5;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Encrypt";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.Encrypt.AutoSize = true;
+            this.Encrypt.Font = new System.Drawing.Font("Sitka Small", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Encrypt.Location = new System.Drawing.Point(192, 254);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(108, 32);
+            this.Encrypt.TabIndex = 5;
+            this.Encrypt.TabStop = true;
+            this.Encrypt.Text = "Encrypt";
+            this.Encrypt.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // Apply
             // 
-            this.button2.BackColor = System.Drawing.Color.DarkSalmon;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Sitka Small", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.button2.Location = new System.Drawing.Point(481, 255);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 36);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Apply";
-            this.button2.UseVisualStyleBackColor = false;
+            this.Apply.BackColor = System.Drawing.Color.DarkSalmon;
+            this.Apply.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Apply.Font = new System.Drawing.Font("Sitka Small", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Apply.Location = new System.Drawing.Point(481, 255);
+            this.Apply.Name = "Apply";
+            this.Apply.Size = new System.Drawing.Size(155, 36);
+            this.Apply.TabIndex = 6;
+            this.Apply.Text = "Apply";
+            this.Apply.UseVisualStyleBackColor = false;
+            this.Apply.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -133,9 +134,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.ClientSize = new System.Drawing.Size(699, 540);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Apply);
+            this.Controls.Add(this.Encrypt);
+            this.Controls.Add(this.Decipher);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.OutputText);
             this.Controls.Add(this.label1);
@@ -158,9 +159,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox OutputText;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton Decipher;
+        private System.Windows.Forms.RadioButton Encrypt;
+        private System.Windows.Forms.Button Apply;
     }
 }
 
